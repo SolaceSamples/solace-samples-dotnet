c6190d0c66e0a043c2ba4b14f1f90df6e8c6d73a
shalin1738 <backupshalin@gmail.com>
2016-10-23 20:17:58 -0400

    add PubSub tutorial to Solace-Samples-dotnet (#5)
    Port code from dev portal.

---
+++ src/TopicSubscriber/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TopicSubscriber")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Solace Systems")]
+[assembly: AssemblyProduct("Tutorial")]
+[assembly: AssemblyCopyright("Copyright © Solace Systems 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e79ca063-2d86-4c32-89b9-4110e2304be0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/TopicSubscriber/TopicSubscriber.cs
@@ -0,0 +1,195 @@
+﻿#region Copyright & License
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+#endregion
+
+using System;
+using System.Text;
+using SolaceSystems.Solclient.Messaging;
+using System.Threading;
+
+/// <summary>
+/// Solace Systems Messaging API tutorial: TopicSubscriber
+/// </summary>
+
+namespace Tutorial
+{
+    /// <summary>
+    /// Demonstrates how to use Solace Systems Messaging API for subscribing to a topic and receiving a message
+    /// </summary>
+    class TopicSubscriber : IDisposable
+    {
+        string VPNName { get; set; }
+        string UserName { get; set; }
+
+        const int DefaultReconnectRetries = 3;
+
+        private ISession Session = null;
+        private EventWaitHandle WaitEventWaitHandle = new AutoResetEvent(false);
+
+        void Run(IContext context, string host)
+        {
+            // Validate parameters
+            if (context == null)
+            {
+                throw new ArgumentException("Solace Systems API context Router must be not null.", "context");
+            }
+            if (string.IsNullOrWhiteSpace(host))
+            {
+                throw new ArgumentException("Solace Messaging Router host name must be non-empty.", "host");
+            }
+            if (string.IsNullOrWhiteSpace(VPNName))
+            {
+                throw new InvalidOperationException("VPN name must be non-empty.");
+            }
+            if (string.IsNullOrWhiteSpace(UserName))
+            {
+                throw new InvalidOperationException("Client username must be non-empty.");
+            }
+
+            // Create session properties
+            SessionProperties sessionProps = new SessionProperties()
+            {
+                Host = host,
+                VPNName = VPNName,
+                UserName = UserName,
+                ReconnectRetries = DefaultReconnectRetries
+            };
+
+            // Connect to the Solace messaging router
+            Console.WriteLine("Connecting as {0}@{1} on {2}...", UserName, VPNName, host);
+            // NOTICE HandleMessage as the message event handler
+            Session = context.CreateSession(sessionProps, HandleMessage, null);
+            ReturnCode returnCode = Session.Connect();
+            if (returnCode == ReturnCode.SOLCLIENT_OK)
+            {
+                Console.WriteLine("Session successfully connected.");
+
+                // This is the topic on Solace messaging router where a message is published
+                // Must subscribe to it to receive messages
+                Session.Subscribe(ContextFactory.Instance.CreateTopic("tutorial/topic"), true);
+
+                Console.WriteLine("Waiting for a message to be published...");
+                WaitEventWaitHandle.WaitOne();
+            }
+            else
+            {
+                Console.WriteLine("Error connecting, return code: {0}", returnCode);
+            }
+        }
+
+        /// <summary>
+        /// This event handler is invoked by Solace Systems Messaging API when a message arrives
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="args"></param>
+        private void HandleMessage(object source, MessageEventArgs args)
+        {
+            Console.WriteLine("Received published message.");
+            // Received a message
+            using (IMessage message = args.Message)
+            {
+                // Expecting the message content as a binary attachment
+                Console.WriteLine("Message content: {0}", Encoding.ASCII.GetString(message.BinaryAttachment));
+                // finish the program
+                WaitEventWaitHandle.Set();
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (Session != null)
+                    {
+                        Session.Dispose();
+                    }
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+
+        #region Main
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solace Systems Messaging API Tutorial, Copyright 2008-2015 Solace Systems, Inc.");
+
+            if ((args.Length < 1) || string.IsNullOrWhiteSpace(args[0]))
+            {
+                Console.WriteLine("Please provide a parameter: non-empty value for the Solace messaging router host name or IP address, e.g. \"TopicSubscriber 192.168.1.111\"");
+                Environment.Exit(1);
+            }
+
+            string host = args[0]; // Solace messaging router host name or IP address
+
+            const string defaultVPNName = "default"; // Solace messaging router VPN name
+            const string defaultUsername = "tutorial"; // client username on the Solace messaging router VPN
+
+            // Initialize Solace Systems Messaging API with logging to console at Warning level
+            ContextFactoryProperties cfp = new ContextFactoryProperties()
+            {
+                SolClientLogLevel = SolLogLevel.Warning
+            };
+            cfp.LogToConsoleError();
+            ContextFactory.Instance.Init(cfp);
+
+            try
+            {
+                // Context must be created first
+                using (IContext context = ContextFactory.Instance.CreateContext(new ContextProperties(), null))
+                {
+                    // Create the application
+                    using (TopicSubscriber topicSubscriber = new TopicSubscriber()
+                    {
+                        VPNName = defaultVPNName,
+                        UserName = defaultUsername,
+                    })
+                    {
+                        // Run the application within the context and against the host
+                        topicSubscriber.Run(context, host);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception thrown: {0}", ex.Message);
+            }
+            finally
+            {
+                // Dispose Solace Systems Messaging API
+                ContextFactory.Instance.Cleanup();
+            }
+            Console.WriteLine("Finished.");
+        }
+        #endregion
+    }
+
+}
